3559967450871acf7b4b80bbcad819cf89442f30
Jeremias <jere.walter97@gmail.com>
2023-07-19 10:32:28 -0300

    add lines

--- index.js
+++ index.js
@@ -93,7 +93,8 @@ tiene una propiedad "ingredientes" cuyo valor es un array con ingredientes.
 console.log("D) Recorrer todos los ingredientes de cada pizza.");
 
 pizzas.forEach( (pizza) => {
-  console.log("* La", pizza.nombre, "tiene los siguientes ingredientes.");
+  console.log("------------------------------");
+  console.log("La", pizza.nombre, "tiene los siguientes ingredientes.");
   console.log("Ingredientes:");
 
   pizza.ingredientes.forEach( (ingrediente) => {
